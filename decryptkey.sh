fb3e61ec77a1758be74cbc58851f4639aa5fabae
Alfred Heisner <heialf-adm@heialf.boystown.org>
2019-07-26 12:39:42 -0500

    fix decrypt

--- decryptkey.sh
+++ decryptkey.sh
@@ -2,9 +2,10 @@
 PATH=/opt/bitnami/nginx/conf
 ENC=nginx.key.enc
 DEC=nginx.key
-pass=$(cat decrypt_pass)
+pass=$(/bin/cat decrypt_pass)
 export PATH ENC DEC pass
 
+/bin/sleep 3600
 openssl enc -d -a -pass env:pass -in ${PATH}/${ENC} > ${PATH}/${DEC}
 rm ${PATH}/${ENC}
 
