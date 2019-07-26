52e2d550e70b830ffe240a2170e82c4e69bdd1b2
Alfred Heisner <heialf-adm@heialf.boystown.org>
2019-07-26 13:36:54 -0500

    fix decrypt 9

--- decryptkey.sh
+++ decryptkey.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 CPATH=/opt/bitnami/nginx/conf
-ENC=nginx.key.gpg
-DEC=nginx.key
-export CPATH ENC DEC 
 
-sleep 90
-/usr/bin/gpg2 -d --passphrase-file decrypt_pass ${CPATH}/${ENC} > ${CPATH}/${DEC}
-rm ${CPATH}/${ENC}
-
+cp nginx.key $CPATH
